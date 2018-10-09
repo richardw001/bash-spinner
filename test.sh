d0e39d0494ba714b5c482f24a3159b65e0eaddd9
richardw001 <richardw001@users.noreply.github.com>
2018-10-09 23:53:46 +0100

    Update test.sh
    current usage will leave spinner running if impatient user uses ctrl-c
    suggest signal traps are internalised on spinner.sh

--- test.sh
+++ test.sh
@@ -4,11 +4,20 @@ source "$(pwd)/spinner.sh"
 
 # test success
 start_spinner 'sleeping for 2 secs...'
+# keep track of spinner in case user does ctrl-c
+spinnerid=$!
+trap "kill -9 $spinnerid" `seq 0 15`
+
 sleep 2
 stop_spinner $?
 
 # test fail
 start_spinner 'copying non-existen files...'
+# keep track of spinner in case user does ctrl-c
+spinnerid=$!
+# kill spinner if signal
+trap "kill -9 $spinnerid" `seq 0 15`
+
 # use sleep to give spinner time to fork and run
 # because cp fails instantly
 sleep 1
